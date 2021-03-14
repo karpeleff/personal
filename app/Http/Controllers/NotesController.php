59c97eab661b8085775957d85bc5c0a4e1cabeb9
User <alekseykarpelev@gmail.com>
2021-03-14 21:55:56 +1000

    beta 2

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Note;
 use App\Models\Action;
-
 use Carbon\Carbon;
 
 class NotesController extends Controller
@@ -13,10 +12,8 @@ class NotesController extends Controller
 
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
-
-
 
     public function index()
     {
@@ -29,9 +26,7 @@ class NotesController extends Controller
     {
         $record = $request->all();
         Note::create($record);
-        //return view('notes/index');
         return  $this->index();
-
     }
 
     public function  getAll()
@@ -40,32 +35,29 @@ class NotesController extends Controller
         return view('notes/all',['data' => $data]);
     }
 
-
     public function  form_add()
     {
-
         return view('notes/add');
     }
 
     public function  createAction(Request $request)
     {
 
-        $mutable = Carbon::now()->addHours(10);
-
+         $mutable = Carbon::now()->addHours(10);
          $record                  =  new Action;
          $record->action          = $request->action;
          $record->local_date_time = $mutable;
          $record->save();
-       //s return view('');
-
-        return  $this->allAction();
+         return  $this->allAction();
     }
 
     public function  allAction()
     {
-        $data = Action::paginate(15);
+        $date = Carbon::now()->addHours(10);
+        $date = substr($date, 0, 10);
+        $data = Action::where('local_date_time', 'like', $date.'%')->simplePaginate(15);
         return view('karina/index',['data' => $data]);
-       // return view('karina/index');
+
     }
 
 
