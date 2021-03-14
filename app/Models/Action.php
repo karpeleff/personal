ac91f88a047025e65dd5245b8ecda41a2d8d55d4
User <alekseykarpelev@gmail.com>
2021-03-14 10:40:24 +1000

    time zone model

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -2,9 +2,17 @@
 
 namespace App\models;
 
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Action extends Model
 {
-    //
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone('Asia/Vladivostok')
+            ->toDateTimeString()
+            ;
+    }
 }
