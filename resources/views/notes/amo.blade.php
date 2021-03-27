e1f96a5c7e47665a612e243c3eec8e88c5ef0204
User <alekseykarpelev@gmail.com>
2021-03-27 12:50:02 +1000

    add amo btn 1

--- resources/views/notes/amo.blade.php
+++ resources/views/notes/amo.blade.php
@@ -1,3 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 window.onload = function () {
 var oauth_scripts = document.querySelectorAll('.amocrm_oauth');
 
@@ -169,35 +178,10 @@ errorCallback(event.data);
 });
 }
 
-//Use:
-//<script
-//   class="amocrm_oauth"
-//   charset="utf-8"
-//   data-client-id="xxxx"
-//   data-title="Button"
-//   data-compact="false"
-//   data-class-name="className"
-//   data-color="red"
-//   data-state="state"
-//   data-error-callback="functionName"
-//   data-mode="popup"
-//   src="https://www.amocrm.ru/auth/button.js"
-// ></script>
-//
-//<script
-//   class="amocrm_oauth"
-//   charset="utf-8"
-//   data-name="Integration name"
-//   data-description="Integration description"
-//   data-redirect_uri="https://example.com"
-//   data-secrets_uri="https://example.com/secrets"
-//   data-logo="https://example.com/amocrm_logo.png"
-//   data-scopes="crm,notifications"
-//   data-title="Button"
-//   data-compact="false"
-//   data-class-name="className"
-//   data-color="red"
-//   data-state="state"
-//   data-mode="post_message"
-//   src="https://www.amocrm.ru/auth/button.js"
-// ></script>
+<script>
+
+</script>
+</body>
+</html>
+
+
