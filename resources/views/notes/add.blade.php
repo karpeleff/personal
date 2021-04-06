f6963a0e295cd6f07bb6893d0fb3a327a1d74919
Aleksey <alekseykarpelev@gmail.com>
2021-04-06 03:54:07 +0000

    edit

--- resources/views/notes/add.blade.php
+++ resources/views/notes/add.blade.php
@@ -10,11 +10,11 @@
                     <form  method="post" action="/notes">
                         @csrf
                         <label for="form_name">Заголовок</label>
-                        <input id="form_name" type="text" name="header" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+                        <input id="form_name" type="text" name="header" class="form-control" placeholder="" required="required" data-error="Firstname is required.">
                         <br><br>
                         <label for="form_message">Сообщение</label>
                       
-                        <textarea id="form_message" name="text" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                        <textarea id="form_message" name="text" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error=""></textarea>
                         <br><br>
                         <input type="hidden" name="img"  value="link" >
                         <input type="submit" class="btn btn-success btn-send" value="Send message">
